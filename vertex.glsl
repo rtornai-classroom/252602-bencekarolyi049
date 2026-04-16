fbd2fe1d94c5608a321c71957d6dfa12e4175b1e
bencekarolyi049 <bencus049@mailbox.unideb.hu>
2026-04-16 22:19:10 +0200

    vertex.glsl

--- vertex.glsl
+++ vertex.glsl
@@ -2,9 +2,9 @@
 
 layout (location = 0) in vec2 aPos;
 uniform mat4 u_projection;
-uniform float u_pointSize; // A pontok átmérője
+uniform float u_pointSize;
 
 void main() {
     gl_Position = u_projection * vec4(aPos, 0.0, 1.0);
-    gl_PointSize = u_pointSize; // Beállítjuk a kért pötty-méretet
+    gl_PointSize = u_pointSize; 
 }
